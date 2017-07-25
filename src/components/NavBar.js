18d5c7179ef6af597b2ec0946edab272afb964eb
JaakkoV <jaakvirt@cs.helsinki.fi>
2017-07-25 22:29:19 +0300

    add npm i --save react-bootstrap and css link to index.html + NavBar sketch in comments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import '../App.css';
-import NavButton from '../containers/NavButton'
+import React, { Component } from "react";
+import "../App.css";
+import NavButton from "../containers/NavButton";
 
 export default class NavBar extends Component {
 
@@ -15,6 +15,56 @@ export default class NavBar extends Component {
       </div>
     )
   }
-
-
 }
+
+/*import { Nav, Navbar, MenuItem, NavItem, NavDropdown } from "react-bootstrap";
+import { connect } from 'react-redux';
+import { toggleVisibility } from '../actions/index';
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="">React-Bootstrap</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1} href="#">
+            Link
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            Link
+          </NavItem>
+          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.4}>Separated link</MenuItem>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
+    );
+  }
+}
+
+
+const mapStateToProps = state => {
+  return {
+    filter: state.filter
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    toggleFilter: (input) => dispatch(
+      toggleVisibility(input)
+    )
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
+*/
