b97af4026023bd0141e0caa60d2dbf461d9c22a5
Jussi Maristo <maot@cs.helsinki.fi>
2017-07-25 15:43:43 +0300

    add points query to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,14 @@ import { FETCH_COURSE_POINTS } from '../actions/index';
 
 export default function(state = {"filter": "Oma sivu"}, action) {
   switch (action.type) {
+    case FETCH_COURSE_POINTS:
+      return Object.assign({}, state,
+        { coursePoints: action.payload.data.length }
+      );
     case 'TOGGLE_WIDGET_VISIBILITY':
-    return Object.assign({}, state,
-      { filter: action.filter.text }
-    );
+      return Object.assign({}, state,
+        { filter: action.filter.text }
+      );
     default: return state;
   }
 }
