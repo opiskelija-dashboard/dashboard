8abce0b96c4eb45faeb8f81d72ff4619b12920dc
Jussi Maristo <maot@cs.helsinki.fi>
2017-07-25 14:02:48 +0300

    add: FilterWidget

---
+++ src/components/FilterWidget.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class FilterWidget extends Component {
+  render() {
+    return (
+      <div className="Widget">
+
+      </div>
+    );
+  }
+}
+
+export default FilterWidget;
