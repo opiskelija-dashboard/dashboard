6aac92b2eefa279cfbe04c4bb7b182126261d13c
Jussi Maristo <maot@cs.helsinki.fi>
2017-07-25 14:58:51 +0300

    remove useless imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
 import '../App.css';
-import UserCoursePoints from '../containers/UserCoursePoints';
 import FilterWidget from './FilterWidget';
-import { toggleVisibility } from '../actions/index';
-import { connect } from 'react-redux';
 import NavBar from './NavBar';
 
 export default class App extends Component {
 
-
-
-
-
   render() {
     return (
-
       <div className="centered">
         <NavBar />
 
