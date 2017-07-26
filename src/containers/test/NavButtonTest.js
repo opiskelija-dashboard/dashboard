106a017b04c0efaee972bd9bbe47b2ba6fead0a8
Jussi Maristo <maot@cs.helsinki.fi>
2017-07-26 15:14:01 +0300

    add button actived when clicked

--- src/containers/test/NavButtonTest.js
+++ src/containers/test/NavButtonTest.js
@@ -8,9 +8,20 @@ import NavButton from '../NavButton'
 
 
 const mockStore = configureStore();
-const initialState = { "filter": "asdasd" };
+const initialState = { "filter": "asdasd"}
 
-test('renders without crashing', t => {
+test('NavButton renders without crashing', t => {
+  const div = document.createElement('div');
+  const store = mockStore(initialState);
+  ReactDOM.render(
+    <Provider store={store}>
+      <NavButton />
+    </Provider>,
+    div
+  );
+});
+
+test('renders button', t => {
   const store = mockStore(initialState);
   const wrapper = mount(
     <Provider store={store}>
