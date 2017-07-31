d7e79b3e6729a3a43b3b33b7b15ac88906ecbd72
Perttu Naumanen <perttu.naumanen@gmail.com>
2017-07-31 16:58:06 +0300

    Reverted chart to functional component

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
 
-export class Chart extends React.Component {
-  render() {
-    return(
+export const Chart = props => (
     <div>
       <h2>Edistyminen</h2>
       <Line
-        data={this.props.data}
+        data={props.data}
         width="auto"
         height="auto"
-        options={this.props.options}
+        options={props.options}
       />
-    </div>)
-  };
-};
+    </div>
+);
