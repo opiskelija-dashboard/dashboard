bcfb0f1096092e2cc67d50e7e8e9e6166f860190
Perttu Naumanen <perttu.naumanen@gmail.com>
2017-08-01 09:09:48 +0300

    cleaned extras from MyPage

--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -4,8 +4,6 @@ import Chart from "../containers/ChartContainer";
 
 export const MyPage = () => (
   <div>
-    <Chart /*progressData={[0, 25, 41, 79, 101, 151, 194, 260, null]} 
-      progressLabels={["Kurssin alku", "Viikko 1", "Viikko 2", "Viikko 3", "Viikko 4", "Viikko 5", "Viikko 6", "Viikko 7", "Viikko 8"]} 
-      maxPoints={330}*/ />
+    <Chart />
   </div>
 )
