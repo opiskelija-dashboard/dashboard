83d80b4b89be400b646174260f46a827f964c6b4
Jaakko Virtanen <JaakkoV@users.noreply.github.com>
2017-08-03 13:14:38 +0300

    merge conflict solved

--- src/components/test/NavBarTest.js
+++ src/components/test/NavBarTest.js
@@ -10,13 +10,8 @@ import ButtonContainer from '../../containers/ButtonContainer';
 
 test('component renders without crashing', t => {
   const mockStore = configureStore();
-<<<<<<< HEAD
-  const initialState = { "filter": "asdasd" }
-=======
   const initialState = {"Widgets": {"filter": "asdasd"}}
->>>>>>> 1d7d61221d132fbe8932e36d17800f9eacf7aa1e
   const store = mockStore(initialState);
-
   const div = document.createElement('div');
   ReactDOM.render(
     <Provider store={store}>
