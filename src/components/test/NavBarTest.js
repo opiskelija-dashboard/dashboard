e75ce27967f8ca94fb266bdd2acc17f94a0c886f
Perttu Naumanen <perttu.naumanen@gmail.com>
2017-08-03 12:57:07 +0300

    App test + folder change

--- src/components/test/NavBarTest.js
+++ src/components/test/NavBarTest.js
@@ -5,12 +5,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 
-import { NavBar } from '../NavBar'
-import ButtonContainer from '../../containers/ButtonContainer'
+import { NavBar } from '../NavBar';
+import ButtonContainer from '../../containers/ButtonContainer';
 
 test('component renders without crashing', t => {
   const mockStore = configureStore();
-  const initialState = { "filter": "asdasd"}
+  const initialState = { "filter": "asdasd" }
   const store = mockStore(initialState);
 
   const div = document.createElement('div');
