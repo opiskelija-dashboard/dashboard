278ba95601e0b61b9e8edf7b3eae4fff9f19692c
JaakkoV <jaakvirt@cs.helsinki.fi>
2017-08-07 21:43:21 +0300

    Create badgecontainer, rm console.log from leaderboard

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -6,7 +6,6 @@ import ReactTable from "react-table";
 import "react-table/react-table.css";
 
 export const LeaderBoard = props => {
-  console.log(props.data);
   return (
     <div>
       <h2>LeaderBoard</h2>
