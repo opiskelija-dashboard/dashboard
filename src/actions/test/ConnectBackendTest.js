2dc95b3ceaecde9567ddbf1370d881eec2d344c1
Perttu Naumanen <perttu.naumanen@gmail.com>
2017-08-08 10:01:34 +0300

    connect backend action test

--- src/actions/test/ConnectBackendTest.js
+++ src/actions/test/ConnectBackendTest.js
@@ -1,8 +1,21 @@
-import test from 'ava'
-import {connectBackend} from '../index.js';
+import test from 'ava';
+import configureMockStore from 'redux-mock-store';
+import * as actions from '../../actions/index.js';
+import sinon from 'sinon';
+import axios from 'axios';
+import ReduxPromise from 'redux-promise';
+import storage from 'store';
 
-test('connectBackend action has correct type ("CONNECT_BACKEND")', t => {
-  t.pass();
+
+test('connect backend action test', t => {
+  var resolved = new Promise((r) => r({ data: { data: { token: 'xyzzy.2nD_'}}}));
+  sinon.stub(axios, "post").returns(resolved);
+  sinon.stub(storage, "get").returns({username:'x', accessToken: 'y'});
+  var middlewares = [ReduxPromise];
+  var mockStore = configureMockStore(middlewares);
+  var expectedActions = [{ type: 'CONNECT_BACKEND', payload: { data: { data: { token: 'xyzzy.2nD_' }}}}];
+  const store = mockStore({});
+  return store.dispatch(actions.connectBackend()).then(() => {
+      t.deepEqual(store.getActions(), expectedActions);
+  })
 });
-
-
