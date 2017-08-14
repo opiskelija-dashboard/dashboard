d2498a83848835ba3bed0f89c260b0f029303f23
Jussi Maristo <maot@cs.helsinki.fi>
2017-08-14 18:32:07 +0300

    fix heatmap

--- src/containers/CalendarContainer.js
+++ src/containers/CalendarContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import {Calendar} from '../components/Calendar'
+import Moment from 'moment'
 // import { connect } from "react-redux";
 
 export default class CalendarContainer extends Component {
@@ -17,25 +18,25 @@ export default class CalendarContainer extends Component {
       { date: "2017-08-12", count: 2}
     ],
     [
-      { date: "2017-08-06", count: 3},
-      { date: "2017-08-07", count: 3},
-      { date: "2017-08-08", count: 2},
-      { date: "2017-08-09", count: 1},
-      { date: "2017-08-10", count: 4},
-      { date: "2017-08-11", count: 2},
-      { date: "2017-08-12", count: 1}
+      { date: "2017-08-13", count: 3},
+      { date: "2017-08-14", count: 3},
+      { date: "2017-08-15", count: 2},
+      { date: "2017-08-16", count: 1},
+      { date: "2017-08-17", count: 4},
+      { date: "2017-08-18", count: 2},
+      { date: "2017-08-19", count: 1}
     ]];
 
     const renderCalendars = weeks.map((v, index) =>
       <div key={index} className="CalendarWeek">
-        <p style={{fontSize: 20}}>Viikkonumero</p>
+        <p style={{fontSize: 20}}>{Moment(v[0].date, "YYYY-MM-DD").week()}</p>
         <Calendar
-          endDate={endDate}
+          endDate={v[6].date}
           numDays={numDays}
           values={v}
         />
         <Calendar
-          endDate={endDate}
+          endDate={v[6].date}
           numDays={numDays}
           values={v}
         />
@@ -45,6 +46,11 @@ export default class CalendarContainer extends Component {
 
     return (
       <div>
+        <div className="CalendarWeek">
+          <p>Viikkonumero</p>
+          <p>Oma aktiivisuutesi</p>
+          <p>Kurssin keskiarvoaktiivisuus</p>
+        </div>
 
         {renderCalendars}
       </div>
