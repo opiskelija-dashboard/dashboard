403c9ab951172d3ee46dd20adcbfc0e734061360
Jussi Maristo <maot@cs.helsinki.fi>
2017-08-24 13:26:52 +0300

    add components to grid

--- src/components/MyPageGrid.js
+++ src/components/MyPageGrid.js
@@ -4,6 +4,8 @@ import "../../node_modules/react-grid-layout/css/styles.css";
 import "../../node_modules/react-resizable/css/styles.css";
 
 import ChartContainer from '../containers/ChartContainer'
+import ProgressBarContainer from "../containers/ProgressBarContainer";
+import CalendarContainer from "../containers/CalendarContainer";
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
@@ -12,13 +14,13 @@ export default class MyPageGrid extends Component {
   render() {
     const layoutLg = [
       {i: 'chart', x: 0, y: 0, w: 10, h: 3},
-      {i: 'skills', x: 1, y: 0, w: 3, h: 2},
-      {i: 'heatmap', x: 4, y: 0, w: 1, h: 2}
+      {i: 'skills', x: 0, y: 4, w: 5, h: 3},
+      {i: 'heatmap', x: 6, y: 4, w: 5, h: 3}
     ];
 
     const layoutMd = [
       {i: 'a', x: 0, y: 0, w: 6, h: 2},
-      {i: 'b', x: 1, y: 0, w: 3, h: 2},
+      {i: 'b', x: 2, y: 0, w: 3, h: 2},
       {i: 'c', x: 4, y: 0, w: 1, h: 2}
     ];
     const layouts = {lg:layoutLg, md: layoutMd}
@@ -31,13 +33,17 @@ export default class MyPageGrid extends Component {
           breakpoints={{lg: 980, sm: 768}}
           cols={{lg: 10, sm: 6}}
         >
-            <div style={{overflow: 'hidden'}} key={'chart'}>
+            <div style={{height: '100%'}} key={'chart'}>
               <ChartContainer />
             </div>
-            <div key={'skills'}>
 
+            <div style={{overflow: 'auto', height: '100%'}} key={'skills'}>
+              <ProgressBarContainer />
             </div>
-            <div key={'heatmap'}>c</div>
+
+            <div style={{height: '100%'}} key={'heatmap'}>
+              <CalendarContainer />
+            </div>
         </ResponsiveReactGridLayout>
       </div>
     )
